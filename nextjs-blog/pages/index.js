848051465921d4a1dc618c0070d3d9ae2c0341dd
Valentina Lacerda <valentinaslacerda07@gmail.com>
2022-11-25 21:30:24 -0300

    first commit

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,24 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          Hi, I'm <strong>Valentina</strong>. I'm a computer scientist. You can
+          contact me on{" "}
+          <a href="https://twitter.com/ValentinaLacer2">Twitter</a>.
+        </p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{" "}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
